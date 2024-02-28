56e1cd9240eaf710a8944e84e81edfe4606662d6
vincentIsGood <vincentdev324@gmail.com>
2024-02-28 17:04:51 +0800

    feat: selection box (w/no functionality)

--- Assets/Scripts/RtsController.cs
+++ Assets/Scripts/RtsController.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 // Double click:
@@ -5,9 +6,22 @@ using UnityEngine;
 public class RtsController: MonoBehaviour{
     public Squad selectedSquads;
 
-    private RaycastHit2D[] hitInfos = new RaycastHit2D[1];
+    private GameObject selectionBox;
+
+    void Awake(){
+        GameObject selectionBoxPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/UI/SelectionBox.prefab");
+        selectionBox = GameObject.Instantiate(selectionBoxPrefab);
+        selectionBox.SetActive(false);
+    }
+
     void Update(){
-        if(Input.GetMouseButtonDown(1)){
+        leftClick();
+        rightClick();
+    }
+
+    private RaycastHit2D[] hitInfos = new RaycastHit2D[1];
+    public void rightClick(){
+        if(Input.GetMouseButton(1)){
             Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             targetPos.z = 0;
             int hitCount = Physics2D.RaycastNonAlloc(targetPos, Vector2.zero, hitInfos);
@@ -18,6 +32,27 @@ public class RtsController: MonoBehaviour{
                     selectedSquads.moveToPos(targetPos, soldier.ownSquad);
                 }else selectedSquads.moveToPos(targetPos);
             }else selectedSquads.moveToPos(targetPos);
+        }
+    }
+
+    private bool clicked = false;
+    private Vector3 clickAnchorPos;
+    public void leftClick(){
+        if(Input.GetMouseButton(0)){
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            if(!clicked){
+                selectionBox.SetActive(true);
+                selectionBox.transform.position = mousePos;
+                selectionBox.transform.localScale = Vector3.zero;
+                clicked = true;
+                clickAnchorPos = mousePos;
+                return;
+            }
+            selectionBox.transform.localScale = mousePos - clickAnchorPos;
+        }else if(Input.GetMouseButtonUp(0)){
+            clicked = false;
+            selectionBox.SetActive(false);
         }
     }
 }
