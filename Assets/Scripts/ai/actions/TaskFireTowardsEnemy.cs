a17f302a21ce089e4cbe788abcd20f94ec98ac78
vincentIsGood <vincentdev324@gmail.com>
2024-03-06 12:31:50 +0800

    chore: fixing stuff imported from private proj

--- Assets/Scripts/ai/actions/TaskFireTowardsEnemy.cs
+++
@@ -1,8 +0,0 @@
-using com.vincentcodes.ai.behaviortree;
-
-public class TaskFireTowardsEnemy: BehaviorTreeNode<SquadBTData>{
-    public override NodeState evaluate(){
-        tree.sharedData.squad.fireTowards(tree.sharedData.target);
-        return state = NodeState.RUNNING;
-    }
-}
--- Assets/Scripts/ai/actions/TaskPursueEnemy.cs
+++
@@ -1,8 +0,0 @@
-using com.vincentcodes.ai.behaviortree;
-
-public class TaskPursueEnemy: BehaviorTreeNode<SquadBTData>{
-    public override NodeState evaluate(){
-        tree.sharedData.squad.moveToPos(tree.sharedData.target.center);
-        return state = NodeState.RUNNING;
-    }
-}
